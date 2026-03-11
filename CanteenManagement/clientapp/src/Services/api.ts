b331add0fe2afd5be59a95b232c70237706422db
Shivam Rai <Shivam Rai@LAPTOP-EMHO1N7Q>
2026-03-11 21:14:11 +0530

    update

--- CanteenManagement/clientapp/src/Services/api.ts
+++ CanteenManagement/clientapp/src/Services/api.ts
@@ -1,10 +1,12 @@
 ﻿export const apiCall = async (url: string, options?: RequestInit) => {
     const base = import.meta.env.BASE_URL;
+    console.log(`Making API call to: ${base}${url}`);
 
     const res = await fetch(`${base}${url}`, {
         headers: { 'Content-Type': 'application/json' },
         ...options,
     });
+    console.log(`Received response:${res}`);
 
     if (!res.ok) {
         throw new Error(`HTTP ${res.status}`);
