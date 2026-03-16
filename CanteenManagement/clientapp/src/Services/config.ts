d23250065c4c757746f27292611b1b852128d9f0
Mukesh <mukesh@rssrsoftwares.com>
2026-03-16 15:42:11 +0530

    update

---
+++ CanteenManagement/clientapp/src/Services/config.ts
@@ -0,0 +1,9 @@
+﻿declare global {
+    interface Window {
+        __BASE_HREF__?: string;
+    }
+}
+
+const baseHref = (window.__BASE_HREF__ || '/').replace(/\/$/, '');
+
+export const API_BASE_URL = baseHref;
