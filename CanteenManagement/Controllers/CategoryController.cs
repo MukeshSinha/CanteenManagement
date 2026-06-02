dc170933f9020ce8bc1b1d752a150966132c40d2
ShivamRai1212 <shivam@rssrsoftwares.com>
2026-06-02 15:46:38 +0530

    update

---
+++ CanteenManagement/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using CanteenManagement.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CanteenManagement.Controllers
+{
+    [Route("api/Category")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        public IHeaderService _headers;
+        private ApiService apiConsume = new ApiService();
+        public CategoryController(IHeaderService headerService)
+        {
+            _headers = headerService;
+        }
+
+        [HttpGet("get-category")]
+        public async Task<IActionResult> getCategory()
+        {
+            string url = ApiService.Canteen + $"Masters/getEmpCategory";
+            var mHeader = _headers.GetHeaders();
+            var response = await apiConsume.SendRequestAsync(url, HttpMethod.Get, mHeader, null);
+            return Content(response, "application/json");
+        }
+    }
+}
