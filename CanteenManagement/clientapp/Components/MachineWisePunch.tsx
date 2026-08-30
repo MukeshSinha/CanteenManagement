41224fd8d7d97d3ba7f57939991476da23fd7ea0
Shivam Rai <Shivam Rai@LAPTOP-EMHO1N7Q>
2026-08-30 17:26:12 +0530

    update

--- CanteenManagement/clientapp/Components/MachineWisePunch.tsx
+++ CanteenManagement/clientapp/Components/MachineWisePunch.tsx
@@ -191,7 +191,7 @@ const MachineWisePunch: React.FC = () => {
                 </Typography>
             </Box>
 
-            {/* Filter Section Card */}
+            {/* Filter Section Cardd */}
             <Card sx={{ mb: 3, borderRadius: 3, boxShadow: '0 4px 20px rgba(0,0,0,0.05)' }}>
                 <CardContent sx={{ p: 2.5 }}>
                     <Stack
